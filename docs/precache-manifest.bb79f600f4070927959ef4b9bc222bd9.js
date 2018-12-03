e0a8976ee39a19620c84811b83ca502848767a84
harutyunyansamo <samo.harutyunyan777@gmail.com>
2018-12-04 03:40:56 +0400

    addes docsx

---
+++ docs/precache-manifest.bb79f600f4070927959ef4b9bc222bd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5db0d643c3108767fee6",
+    "url": "/static/js/main.5db0d643.chunk.js"
+  },
+  {
+    "revision": "ed1f0f08d350004fce40",
+    "url": "/static/js/1.ed1f0f08.chunk.js"
+  },
+  {
+    "revision": "5db0d643c3108767fee6",
+    "url": "/static/css/main.c5b651c5.chunk.css"
+  },
+  {
+    "revision": "1c8f8261b96a198498e2251477592bac",
+    "url": "/index.html"
+  }
+];
