d31196c30e95cea38e39d6fea112fd45e395775c
harutyunyansamo <samo.harutyunyan777@gmail.com>
2018-12-09 02:46:18 +0400

    Updates

--- precache-manifest.cf6f360ed8fc60cc9bd58cdb70efff22.js
+++ precache-manifest.cf6f360ed8fc60cc9bd58cdb70efff22.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/rc-nutrition-calculator/static/js/runtime~main.10722dd5.js"
   },
   {
-    "revision": "8d5192f86ff7e4f7f205",
-    "url": "/rc-nutrition-calculator/static/js/main.8d5192f8.chunk.js"
+    "revision": "cdffe40220b1c5e65946",
+    "url": "/rc-nutrition-calculator/static/js/main.cdffe402.chunk.js"
   },
   {
     "revision": "26080d2458708f9aae58",
     "url": "/rc-nutrition-calculator/static/js/1.26080d24.chunk.js"
   },
   {
-    "revision": "8d5192f86ff7e4f7f205",
+    "revision": "cdffe40220b1c5e65946",
     "url": "/rc-nutrition-calculator/static/css/main.6db4d3c4.chunk.css"
   },
   {
-    "revision": "60397697117ed967a2dcc57ac7605d55",
+    "revision": "2d85fc1bec2e6210032b921008481dab",
     "url": "/rc-nutrition-calculator/index.html"
   }
 ];
