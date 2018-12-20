0379032f66e79406be6af8d2bda73f810a6dc871
Luke Connolly <luke@deluko.com>
2018-12-20 11:59:59 -0500

    Update step titles microcopy per client request

--- src/components/Steps/index.js
+++ src/components/Steps/index.js
@@ -3,7 +3,7 @@ import Step2 from "./Step2.jsx";
 import Step3 from "./Step3.jsx";
 
 Step1.title = "Pick Your Mix";
-Step2.title = "What’s Your Recipe";
-Step3.title = "Nutrition Breakdown";
+Step2.title = "Add Your Recipe";
+Step3.title = "View Nutrition Info";
 
 export default { Step1, Step2, Step3 };
